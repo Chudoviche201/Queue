3c698bad29b2d024ee31b6d21c439df278fa6633
Vlad Hat <moki.hat@mail.ru>
2023-06-09 19:15:37 +0700

    Добавьте файлы проекта.

---
+++ Queue/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        // создание очереди
+        Queue myQueue = new Queue();
+
+        // добавление элементов
+        myQueue.Enqueue("John");
+        myQueue.Enqueue("Jane");
+        myQueue.Enqueue("Bob");
+
+        // удаление элемента
+        object dequeuedItem = myQueue.Dequeue();
+
+        Console.WriteLine("dequeuedItem: {0}", dequeuedItem);
+
+        // просмотр элемента в начале очереди, без удаления
+        object peekedItem = myQueue.Peek();
+
+        Console.WriteLine("peekedItem: {0}", peekedItem);
+
+        // перебор элементов очереди
+        foreach (object item in myQueue)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.ReadLine();
+    }
+}
